ad3ba8ebe72218f857ea39f895487ffe4d78e48c
LuanNT <ngtluan2k@gmail.com>
2025-09-12 15:54:14 +0700

    fix: config env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,12 +10,14 @@ import { RoleModule } from './modules/role/role.module';
 import { PermissionModule } from './modules/permission/permission.module';
 import { RolePermissionModule } from './modules/role-permission/role-permission.module';
 import { UserRoleModule } from './modules/user-role/user-role.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     // Đọc file .env
     ConfigModule.forRoot({
       isGlobal: true, // để tất cả module khác đều dùng được
+      envFilePath: join(process.cwd(), 'backend', 'src', 'config', '.env'),
     }),
 
     // Cấu hình DB dùng ConfigService
@@ -31,6 +33,8 @@ import { UserRoleModule } from './modules/user-role/user-role.module';
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
         synchronize: false, //change to true if you want to start server;
+        retryAttempts: 10,   // số lần thử kết nối lại (mặc định 10)
+        retryDelay: 10000,    // thời gian delay mỗi lần thử (ms, mặc định 3000)..
       }),
     }),
 
